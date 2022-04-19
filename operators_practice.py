6850567c20960512e23fe03ef40954a21e81e3fe
HtH-HacktheHood <74259570+HtH-HacktheHood@users.noreply.github.com>
2022-04-19 17:25:02 -0400

    updating repo for week 4

--- operators_practice.py
+++ operators_practice.py
@@ -4,12 +4,12 @@ Below are three set's of numbers. Create three variables that are equal to each 
 set1, set2, set3, = [25, 10], [50, 27], [61, 38]
 
 '''
-Below are three set's of numbers. Calculate the result of when the first number entered is raised to the power of the second number entered.
+Below are three set's of numbers. Create three variables and make them equal to the result of when the first number entered from one set is raised to the power of the second number.
 '''
 set4, set5, set6, = [2, 3], [10, 7], [5, 4]
 
 '''
-Below are three set's of numbers. Calculate how many times the second number can be divided into the first number. For example if the first number entered was 23 and the second number entered was 4 the program should report 5 times
+Below are three set's of numbers.  Create three variables and make them equal to the number of times the second number can be divided into the first number. For example if the first number entered was 23 and the second number entered was 4 the program should report 5 times
 '''
 set7, set8, set9, = [23, 4], [19, 3], [81, 13]
 
--- some_work.py
+++
@@ -1,4 +0,0 @@
-nums = [1,2,3,4,5,6,7,8,9]
-
-for num in nums:
-    print(num)
